2827fb510fa1730e8d6f715068348e24f2d78d0e
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-04 14:34:55 -0300

    :heavy_plus_sign: add headlessui; :recycle: refactor: remove onclick

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,19 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
-
   return (
-    <div className="absolute right-5 bottom-5">
-      { isWidgetOpen && <p>Hello World!</p> }
+    <Popover className="absolute right-5 bottom-5">
+      <Popover.Panel>Hello World!</Popover.Panel>
 
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full p-4 text-white flex items-center group">
+      <Popover.Button className="bg-brand-500 rounded-full p-4 text-white flex items-center group">
         <ChatTeardropDots className="w-8 h-8" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
 
