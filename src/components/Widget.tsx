3f7e21feeb29bd88df4c1fff343c03dfc06441a1
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-04 14:27:30 -0300

    :sparkles: feat: widget button onclick

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
   return (
     <div className="absolute right-5 bottom-5">
-      <button className="bg-brand-500 rounded-full p-4 text-white flex items-center group">
+      { isWidgetOpen && <p>Hello World!</p> }
+
+      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full p-4 text-white flex items-center group">
         <ChatTeardropDots className="w-8 h-8" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
