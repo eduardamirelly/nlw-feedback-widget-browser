75c6c89175cd9da20f5671bfea5ddc544cbbfaed
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-04 23:56:08 -0300

    :sparkles: feat: create WidgetForm.tsx and CloseButton.tsx components

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,13 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import WidgetForm from './WidgetForm'
 
 export function Widget() {
   return (
-    <Popover className="absolute right-5 bottom-5">
-      <Popover.Panel>Hello World!</Popover.Panel>
+    <Popover className="absolute right-5 bottom-5 md:right-8 md:bottom-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
       <Popover.Button className="bg-brand-500 rounded-full p-4 text-white flex items-center group">
         <ChatTeardropDots className="w-8 h-8" />
