dfb56f997ab049388d546b4d19bdd8717deaf49a
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-05 17:45:43 -0300

    :sparkles: feat: FeedbackTypeStep.tsx

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-
-import CloseButton from "../CloseButton"
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep"
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep"
 
 import bugImageUrl from '../../assets/Bug.svg'
 import ideaImageUrl from '../../assets/Idea.svg'
 import thoughtImageUrl from '../../assets/Thought.svg'
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -30,7 +30,7 @@ const feedbackTypes = {
   }
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   
@@ -38,29 +38,10 @@ export function WidgetForm() {
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {!feedbackType ? (
-				<div className="flex py-8 gap-2 w-full">
-					{ Object.entries(feedbackTypes).map(([key, value]) => {
-						return(
-							<button 
-								className="flex flex-1 flex-col items-center bg-zinc-800 rounded-lg py-5 w-24 gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
-								key={key} 
-								type="button"
-								onClick={() => setFeedbackType(key as FeedbackType)}
-							>
-									<img src={value.image.source} alt={value.image.alt} />
-									<span>{value.title}</span>
-							</button>
-						)
-					}) }
-				</div>
+				<FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
 			) : (
-				<p>Hello World!</p>
+				<FeedbackContentStep />
 			)}
 
       <footer className="text-xs text-neutral-400">
