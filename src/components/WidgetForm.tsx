74ab3f7fd1f7f16e66eb1a87fa322e770f6927ab
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-05 00:06:18 -0300

    :label: create type FeedbackType

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import CloseButton from "./CloseButton"
 
 import bugImageUrl from '../assets/Bug.svg'
@@ -28,7 +30,12 @@ const feedbackTypes = {
   }
 }
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  
+	const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -36,16 +43,25 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        { Object.entries(feedbackTypes).map(([key, value]) => {
-          return(
-            <button className="flex flex-1 flex-col items-center bg-zinc-800 rounded-lg py-5 w-24 gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" key={key} type="button">
-                <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
-            </button>
-          )
-        }) }
-      </div>
+      {!feedbackType ? (
+				<div className="flex py-8 gap-2 w-full">
+					{ Object.entries(feedbackTypes).map(([key, value]) => {
+						return(
+							<button 
+								className="flex flex-1 flex-col items-center bg-zinc-800 rounded-lg py-5 w-24 gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
+								key={key} 
+								type="button"
+								onClick={() => setFeedbackType(key as FeedbackType)}
+							>
+									<img src={value.image.source} alt={value.image.alt} />
+									<span>{value.title}</span>
+							</button>
+						)
+					}) }
+				</div>
+			) : (
+				<p>Hello World!</p>
+			)}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
