c8bc445c1587063a67f675e70409e94e04a0bbda
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-07 01:12:50 -0300

    :recycle: refactor: apply solid concepts on send email

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,20 @@
 import express from 'express'
-import nodemailer from 'nodemailer';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
-const transport = nodemailer.createTransport({
-	host: "smtp.mailtrap.io",
-	port: 2525,
-	auth: {
-		user: "d06ca115164475",
-		pass: "0b65bc042625de"
-	}
-});
-
 routes.post('/feedbacks', async (req, res) => {
 	const { type, comment, screenshot } = req.body;
 
 	const prismaFeedbackRepository = new PrismaFeedbacksRepository();
+	const nodemailerMailAdapter = new NodemailerMailAdapter();
+
 	const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-		prismaFeedbackRepository
+		prismaFeedbackRepository,
+		nodemailerMailAdapter
 	);
 
 	await submitFeedbackUseCase.execute({
@@ -28,18 +22,6 @@ routes.post('/feedbacks', async (req, res) => {
 		comment, 
 		screenshot,
 	});
-
-	// await transport.sendMail({
-	// 	from: 'Equipe Feedget <oi@feedget.com>',
-	// 	to: 'Eduarda Mirelly <mirelly.eduarda404@gmail.com>',
-	// 	subject: 'Novo feedback',
-	// 	html: [
-	// 		`<div style="font-familly: sans-serif; font-size: 16px; color: #111;">`,
-	// 		`<p>Tipo do feedback: ${type}</p>`,
-	// 		`<p>Comentário: ${comment}</p>`,
-	// 		`</div>`,
-	// 	].join('\n')
-	// });
 
 	return res.status(201).send()
 });
--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,4 +1,5 @@
 import { FeedbacksRepository } from "../repositories/feedback-repository";
+import { MailAdapter } from "../adapters/mail-adapter";
 
 interface SubmitFeedbackUseCaseRequest {
   type: string;
@@ -8,7 +9,8 @@ interface SubmitFeedbackUseCaseRequest {
 
 export class SubmitFeedbackUseCase {
   constructor(
-    private feedbacksRepository: FeedbacksRepository
+    private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter,
   ){}
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
@@ -18,6 +20,16 @@ export class SubmitFeedbackUseCase {
       type,
       comment,
       screenshot,
+    })
+
+    await this.mailAdapter.sendMail({
+      subject: 'Novo feedback',
+      body: [
+        `<div style="font-familly: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        `</div>`,
+      ].join('\n')
     })
   }
 }
